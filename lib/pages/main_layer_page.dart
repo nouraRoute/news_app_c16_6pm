e8a89da1a4f3748109765f05b6fd7f99aeaed3b0
nouraRoute <noura.route@gmail.com>
2025-10-15 20:38:44 +0300

    category details

--- lib/pages/main_layer_page.dart
+++ lib/pages/main_layer_page.dart
@@ -1,40 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:news_app_c16_6pm/common/extention/theme_extention.dart';
-import 'package:news_app_c16_6pm/enums/category_enum.dart';
-import 'package:news_app_c16_6pm/pages/widgets/category_card.dart';
+import 'package:news_app_c16_6pm/pages/views/category_details_view.dart';
+import 'package:news_app_c16_6pm/pages/views/category_view.dart';
+import 'package:news_app_c16_6pm/pages/views/home_dreawer.dart';
+import 'package:news_app_c16_6pm/providers/category_provider.dart';
+import 'package:provider/provider.dart';
 
 class MainLayerPage extends StatelessWidget {
   const MainLayerPage({super.key});
   static const String routeName = '/mainLayerPage';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(),
-      appBar: AppBar(
-        title: Text(
-          'Home', //TODO:localization
-        ),
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '''Good Morning
-Here is Some News For You''',
-              style: context.getTextTheme.titleMedium!.copyWith(fontSize: 24),
-            ),
-            ...List.generate(
-              CategoryEnum.values.length,
-              (index) => CategoryCard(
-                categoryEnum: CategoryEnum.values[index],
-                isRight: (index % 2 == 0),
+    return ChangeNotifierProvider(
+      create: (context) => CategoryProvider(),
+      child: Consumer<CategoryProvider>(
+        builder: (BuildContext context, CategoryProvider value, Widget? child) {
+          return Scaffold(
+            drawer: HomeDrawer(),
+            appBar: AppBar(
+              title: Text(
+                value.selectedCategory != null
+                    ? value.selectedCategory!.name
+                    : 'Home', //TODO:localization
               ),
+              actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
             ),
-          ],
-        ),
+            body: value.selectedCategory != null
+                ? CategoryDetailsView()
+                : CategoryListView(),
+          );
+        },
       ),
     );
   }
